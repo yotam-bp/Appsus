44ddc02201933f62599d6fb552de236ab6c7e73c
yotam-bp <yotyyybp@gmail.com>
2021-05-04 20:57:51 +0300

    small changes

--- apps/Keeps/services/note.service.js
+++ apps/Keeps/services/note.service.js
@@ -52,3 +52,48 @@ function getNoteById(noteId) {
     var note = storageService.loadFromStorage('notes').find(note => note.id === noteId)
     return Promise.resolve(note)
 }
+
+// function deleteCar(carId) {
+//     var carIdx = gCars.findIndex(function (car) {
+//         return carId === car.id
+//     })
+//     gCars.splice(carIdx, 1)
+//     _saveCarsToStorage();
+
+//     return Promise.resolve()
+// }
+// function saveCar(car) {
+//     return car.id ? _updateCar(car) : _addCar(car)
+// }
+// function _addCar(carToAdd) {
+//     var car = _createCar(carToAdd.vendor, carToAdd.speed)
+//     gCars.unshift(car)
+//     _saveCarsToStorage();
+//     return Promise.resolve(car)
+// }
+
+// function _updateCar(carToUpdate) {
+//     var carIdx = gCars.findIndex(function (car) {
+//         return car.id === carToUpdate.id;
+//     })
+//     gCars.splice(carIdx, 1, carToUpdate)
+//     _saveCarsToStorage();
+//     return Promise.resolve(carToUpdate)
+// }
+
+// function getNextCarId(carId) {
+//     const carIdx = gCars.findIndex(car => car.id === carId)
+//     var nextCarIdx = carIdx + 1
+//     nextCarIdx = nextCarIdx === gCars.length ? 0 : nextCarIdx
+//     return gCars[nextCarIdx].id
+// }
+// function _createCar(vendor, speed) {
+//     if (!speed) speed = utilService.getRandomIntInclusive(1, 200)
+//     return {
+//         id: utilService.makeId(),
+//         vendor,
+//         speed,
+//         desc: utilService.makeLorem(),
+//         isBestSeller : Math.random() > 0.5
+//     }
+// }
