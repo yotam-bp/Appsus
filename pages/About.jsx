1639f1b874a7c5a9bca1f01780b73d022de98aa7
yotam-bp <yotyyybp@gmail.com>
2021-05-04 18:32:33 +0300

    About team

--- pages/About.jsx
+++ pages/About.jsx
@@ -5,8 +5,8 @@ export function About() {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed rem in quaerat earum fugit harum saepe eveniet exercitationem, officiis, eaque aut. At autem, expedita impedit modi eum debitis laborum aperiam? Modi laboriosam eligendi laborum consectetur aut nobis, voluptates, pariatur dolorum nam praesentium provident. Nesciunt, ducimus! Sed adipisci quas facere optio.
             <section className="team-contaier">
                 <div className="team-member">
-                    <img className="profile-img"/>
-                    <p>This is Yotam</p>
+                    <img className="profile-img" src="../assets/img/yotam.JPG"/>
+                    <h3>Hi! I'm Yotam</h3>
                 </div>
                 <div className="team-member">
                     <h3>Hi! I'm Naama</h3>
