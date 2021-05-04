5cd06fd18e931b57b5bbf3201314256715887063
Naama Weber <naamaw99@gmail.com>
2021-05-04 16:46:58 +0300

    added Naama's about

--- pages/About.jsx
+++ pages/About.jsx
@@ -5,12 +5,15 @@ export function About() {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed rem in quaerat earum fugit harum saepe eveniet exercitationem, officiis, eaque aut. At autem, expedita impedit modi eum debitis laborum aperiam? Modi laboriosam eligendi laborum consectetur aut nobis, voluptates, pariatur dolorum nam praesentium provident. Nesciunt, ducimus! Sed adipisci quas facere optio.
             <section className="team-contaier">
                 <div className="team-member">
-                    <img />
+                    <img className="profile-img"/>
                     <p>This is Yotam</p>
                 </div>
                 <div className="team-member">
-                    <img />
-                    <p>This is Naama</p>
+                    <h3>Hi! I'm Naama</h3>
+                    <img className="profile-img" src="../assets/img/naama-img.jpeg"/>
+                    <p>
+                        I'm 26 years old, living in Tel Aviv. In my free time I love baking, working out and Coding of course! I'm an aspiring programer and this is my first big React app!
+                    </p>
                 </div>
             </section>
         </section>
