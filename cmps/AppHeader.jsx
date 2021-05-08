35648162a387d2139098586ff5e34fed9a8e23aa
Naama Weber <naamaw99@gmail.com>
2021-05-09 00:16:37 +0300

    hamburger

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -6,13 +6,14 @@ export function AppHeader() {
     return (
         <nav className="app-header">
             <h1 className="logo"><span>A</span>ppsus</h1>
-            <div className="btn-menu">☰
+            <div className="btn-menu">
+            <span>☰</span>
             <ul className="main-nav clean-list">
-                <li><NavLink exact to="/" activeStyle={{ color: '#d87361' }}>Home</NavLink></li>
-                <li><NavLink to="/about" activeStyle={{ color: '#d87361' }}>About</NavLink></li>
-                <li><NavLink to="/miss-books" activeStyle={{ color: '#d87361' }}>Books</NavLink></li>
-                <li><NavLink to="/miss-keep" activeStyle={{ color: '#d87361' }}>Notes</NavLink></li>
-                <li><NavLink to="/mister-email" activeStyle={{ color: '#d87361' }}>Email</NavLink></li>
+                <li><NavLink exact to="/" activeStyle={{ color: '#159097' }}>Home</NavLink></li>
+                <li><NavLink to="/about" activeStyle={{ color: '#159097' }}>About</NavLink></li>
+                <li><NavLink to="/miss-books" activeStyle={{ color: '#159097' }}>Books</NavLink></li>
+                <li><NavLink to="/miss-keep" activeStyle={{ color: '#159097' }}>Notes</NavLink></li>
+                <li><NavLink to="/mister-email" activeStyle={{ color: '#159097' }}>Email</NavLink></li>
             </ul>
             </div>
         </nav>
